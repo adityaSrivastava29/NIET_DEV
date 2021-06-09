5a6a3502878a22eeb2b7c522cdc3cc4ecb48a5ac
sushant <sushantberiwal@gmail.com>
2021-06-09 10:54:29 +0530

    functions and hoisting

--- Module1/Lec1_Intro/intro.js
+++ Module1/Lec1_Intro/intro.js
@@ -87,7 +87,7 @@ let avenger = {
   age: 200,
 };
 
-console.log(avenger.skills[2]["BestFriend"][1].skills[0].substring(4)); 
+console.log(avenger.skills[2].BestFriend[1].skills[0].substring(4)); 
 
 
 
